feec442d091fa57986847d8ede6c70306747493d
Hadrien G <knights_of_ni@gmx.com>
2019-11-12 15:40:53 +0100

    Add memory alignment support

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #![recursion_limit="128"]
 
-use synstructure::decl_derive;
 use quote::quote;
+use std::collections::HashSet;
+use synstructure::decl_derive;
 
 decl_derive!([Abomonation, attributes(unsafe_abomonate_ignore)] => derive_abomonation);
 
@@ -17,15 +18,40 @@ fn derive_abomonation(mut s: synstructure::Structure) -> proc_macro2::TokenStrea
         ::abomonation::Entomb::entomb(#bi, _write)?;
     });
 
-    let extent = s.fold(
-        quote!(0),
-        |acc, bi| quote!(#acc + ::abomonation::Entomb::extent(#bi))
-    );
+    // T::alignment() is the max of mem::align_of<T> and the U::alignment()s of
+    // every U type used as a struct member or inside of an enum variant (which
+    // includes the alignment of recursively abomonated data)
+    //
+    // Unfortunately, we cannot use synstructure's nice `fold()` convenience
+    // here because it's based on generating match arms for a `self` value,
+    // whereas here we're trying to implement an inherent type method without
+    // having such a `self` handy.
+    //
+    // We can, however, use Structure::variants() and VariantInfo::bindings()
+    // to enumerate all the types which _would appear_ in such match arms'
+    // inner bindings.
+    //
+    let mut alignment = vec![
+        quote!(let mut align = ::std::mem::align_of::<Self>();)
+    ];
+    let mut probed_types = HashSet::new();
+    for variant_info in s.variants() {
+        for binding_info in variant_info.bindings() {
+            let binding_type = &binding_info.ast().ty;
+            // Do not query a type's alignment() multiple times
+            if probed_types.insert(binding_type) {
+                alignment.push(
+                    quote!(align = align.max(<#binding_type as ::abomonation::Entomb>::alignment());)
+                );
+            }
+        }
+    }
+    alignment.push(quote!(align));
 
     s.bind_with(|_| synstructure::BindStyle::RefMut);
 
     let exhume = s.each(|bi| quote! {
-        bytes = ::abomonation::Exhume::exhume(From::from(#bi), bytes)?;
+        ::abomonation::Exhume::exhume(From::from(#bi), reader)?;
     });
 
     s.gen_impl(quote! {
@@ -33,13 +59,16 @@ fn derive_abomonation(mut s: synstructure::Structure) -> proc_macro2::TokenStrea
         extern crate std;
 
         gen unsafe impl abomonation::Entomb for @Self {
-            unsafe fn entomb<W: ::std::io::Write>(&self, _write: &mut W) -> ::std::io::Result<()> {
+            unsafe fn entomb<W: ::std::io::Write>(
+                &self,
+                _write: &mut ::abomonation::align::AlignedWriter<W>
+            ) -> ::std::io::Result<()> {
                 match *self { #entomb }
                 Ok(())
             }
 
-            fn extent(&self) -> usize {
-                match *self { #extent }
+            fn alignment() -> usize {
+                #(#alignment)*
             }
         }
 
@@ -49,13 +78,13 @@ fn derive_abomonation(mut s: synstructure::Structure) -> proc_macro2::TokenStrea
             #[allow(unused_mut)]
             unsafe fn exhume(
                 self_: std::ptr::NonNull<Self>,
-                mut bytes: &'de mut [u8]
-            ) -> Option<&'de mut [u8]> {
+                reader: &mut ::abomonation::align::AlignedReader<'de>
+            ) -> Option<&'de mut Self> {
                 // FIXME: This (briefly) constructs an &mut _ to invalid data
                 //        (via "ref mut"), which is UB. The proposed &raw mut
                 //        operator would allow avoiding this.
                 match *self_.as_ptr() { #exhume }
-                Some(bytes)
+                Some(&mut *self_.as_ptr())
             }
         }
     })
